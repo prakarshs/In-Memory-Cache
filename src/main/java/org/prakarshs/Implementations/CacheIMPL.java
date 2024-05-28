33b46c0f0653e350f929231639fe4ed371de9e73
jhethia <jhethia@gmail.com>
2024-05-28 10:31:53 +0530

    created CacheValue

--- src/main/java/org/prakarshs/Implementations/CacheIMPL.java
+++ src/main/java/org/prakarshs/Implementations/CacheIMPL.java
@@ -4,10 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.prakarshs.Interfaces.CacheInterface;
+
+import java.util.concurrent.ConcurrentHashMap;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class CacheIMPL<K,V> implements CacheInterface<K,V> {
+    ConcurrentHashMap<K,>
     @Override
     public void put(K key, V value) {
 
